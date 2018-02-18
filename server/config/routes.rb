d572fe903f6cf3fea53a8e3cf1d57e162c68b98a
Brent Younce <bjyounce@ncsu.edu>
2018-02-18 11:28:10 -0500

    Update routes.rb for login

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
 
   get 'home/index'
 
+  get    '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
   resources :companies
   resources :users
-  resources :applications
+
 
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
