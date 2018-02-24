9ba79562ccda17595949133eabdcff8e97c35d70
Wayne Chen <cchen31@ncsu.edu>
2018-02-23 19:45:22 -0500

    phone field for job seeker and job seeker can't view others applications

--- server/app/controllers/users_controller.rb
+++ server/app/controllers/users_controller.rb
@@ -71,6 +71,6 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:email, :password, :role, :company_id,
-                                 :document, :name)
+                                 :document, :name, :phone)
   end
 end
